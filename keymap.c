d7b5a8540ba45867ea153bf258d7148ab42dadc3
Dani <danielmartin.hermoso.hermoso.alu@iesfernandoaguilar.es>
2023-10-22 21:15:26 +0200

    added layer game

--- keymap.c
+++ keymap.c
@@ -22,6 +22,7 @@ enum sofle_layers {
     _LOWER,
     _RAISE,
     _ADJUST,
+    _GAME,
 };
 
 // clang-format off
@@ -153,11 +154,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT(
     QK_BOOT, RGB_M_P , RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN,                        RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______, _______,
-    _______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______,                           _______, _______, _______, _______, _______, _______,
+    _______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______,                           _______, _______, _______, _______, _______, TO(_GAME),
     _______, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_MOD,                           RGB_RMOD, _______, _______, _______, _______, _______,
     TO(_QWERTY), _______, _______, _______, _______, RGB_TOG, _______,     _______, _______, _______, _______, _______, _______, QK_BOOT,
                     _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______
+),
+
+[_GAME] = LAYOUT(
+    KC_ESC,         KC_1,           KC_2,           KC_3,    KC_4,    KC_5,                                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
+    KC_TAB,         KC_Q,           KC_W,           KC_E,    KC_R,    KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BACKSLASH,
+    KC_CAPS_LOCK,   KC_A,           KC_S,           KC_D,    KC_F,      KC_G,                                       KC_H, KC_J, KC_K, KC_L, KC_SCLN,   KC_QUOT,
+    KC_LSFT,        KC_Z,           KC_X,           KC_C,    KC_V,    KC_B,     KC_BTN1,                KC_BTN2,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+                                    KC_LGUI,        KC_LALT, KC_LCTL, KC_SPC, KC_ENTER,        LT(_RAISE,KC_SPC), KC_ENTER, KC_RCTL, KC_RALT, C(KC_S)
 )
+
+
+
+
 };
 // clang-format on
 
